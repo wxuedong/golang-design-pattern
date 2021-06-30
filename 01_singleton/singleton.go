5d422ad79ac34a57c01a26094a23b9f10fad19e5
1900 <wxuedon@gmail.com>
2021-06-30 20:28:09 +0800

    Create singleton design pattern

--- 01_singleton/singleton.go
+++ 01_singleton/singleton.go
@@ -4,3 +4,17 @@
   @Note:
 **/
 package _1_singleton
+
+type singleton struct {
+}
+
+// private
+var instance *singleton
+
+// public
+func GetInstance() *singleton {
+	if instance == nil {
+		instance = &singleton{}     // not thread safe
+	}
+	return instance
+}
