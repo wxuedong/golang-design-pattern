f51b96260cae926cf282ae5fbd55ed6c288b3d7e
1900 <wxuedon@gmail.com>
2021-07-20 19:17:24 +0800

    Create bridge design pattern

--- 06_bridge/bridge_test.go
+++ 06_bridge/bridge_test.go
@@ -4,3 +4,16 @@
   @Note:
 **/
 package bridge
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestErrorNotification_Notify(t *testing.T) {
+	sender := NewEmailMsgSender([]string{"test@test.com"})
+	n := NewErrorNotification(sender)
+	err := n.Notify("test msg")
+
+	assert.Nil(t, err)
+}
