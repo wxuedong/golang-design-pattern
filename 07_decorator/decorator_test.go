7cdd1856dff3790b987b96856bd1ee9ffb48446a
1900 <wxuedon@gmail.com>
2021-08-04 22:51:06 +0800

    Create decorator design pattern

--- 07_decorator/decorator_test.go
+++ 07_decorator/decorator_test.go
@@ -4,3 +4,15 @@
   @Note:
 **/
 package decorator
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestColorSquare_Draw(t *testing.T) {
+	sq := Square{}
+	csq := NewColorSquare(sq, "red")
+	got := csq.Draw()
+	assert.Equal(t, "this is a square, color is red", got)
+}
