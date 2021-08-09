870ce4d2c417269c8a7260b1bd816053f23ec2fd
1900 <wxuedon@gmail.com>
2021-08-09 22:07:28 +0800

    Create decorator design pattern

--- 07_decorator/decorator_test.go
+++ 07_decorator/decorator_test.go
@@ -10,3 +10,9 @@ import (
 	"testing"
 )
 
+func TestColorSquare_Draw(t *testing.T) {
+	sq := Square{}
+	csq := NewColorSquare(sq, "red")
+	got := csq.Draw()
+	assert.Equal(t, "this is a square, color is red", got)
+}
